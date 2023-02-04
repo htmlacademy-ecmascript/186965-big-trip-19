cd758ec15e54e91da5cedf94bb25afcbc87d084e
Victoria <vitagood.92@list.ru>
2023-02-04 17:45:58 +0300

    Добавит презентер фильтров

--- src/main.js
+++ src/main.js
@@ -1,48 +1,42 @@
 // View
 import NewTripMainInfoBlockView from './view/header/trip-info-block-view.js';
-import NewTripControlsFiltersView from './view/header/trip-controls-filters-view.js';
 
 // Presenter
 import HeaderPresenter from './presenter/header/header-presenter.js';
 import TripBoardPresenter from './presenter/main/trip-events-presenter.js';
+import FilterPresenter from './presenter/header/filter-presenter.js';
 
 // Model
 import TripPointModel from './models/trip-point-model.js';
+import FilterModel from './models/filter-model.js';
 
 import { render, RenderPosition } from './framework/render.js';
 
 //mock
-import { generateFilter } from './mock/filter.js';
-
-
-const filters = [
-  {
-    type: 'all',
-    name: 'ALL',
-    count: 0,
-  },
-];
 
 const tripMainElement = document.querySelector('.trip-main');
 const tripControlsFiltersElement = tripMainElement.querySelector('.trip-controls');
 const tripsEventsContainerElement = document.querySelector('.trip-events');
 
 const pointModel = new TripPointModel();
+const filterModel = new FilterModel();
 
 //header
 
 render(new NewTripMainInfoBlockView(), tripMainElement, RenderPosition.AFTERBEGIN);
 
-render(new NewTripControlsFiltersView({
-  filters,
-  currentFilterType: 'all',
-  onFilterTypeChange: () => { }
-}), tripControlsFiltersElement);
+
+const filterPresenter = new FilterPresenter({
+  filterContainer: tripControlsFiltersElement,
+  filterModel,
+  pointModel
+});
+
 
 const headerPresenter = new HeaderPresenter(tripMainElement, pointModel);
 
 headerPresenter.init();
-
+filterPresenter.init();
 
 //main
 const mainPresenter = new TripBoardPresenter(tripsEventsContainerElement);
--- src/mock/filter.js
+++
@@ -1,9 +0,0 @@
-import { filterWaypointValue } from '../utils/filter.js';
-
-const generateFilter = (points) => Object.entries(filterWaypointValue).map(([filterName, filterPoints]) => ({
-  name: filterName,
-  count: filterPoints(points).length,
-}));
-
-
-export { generateFilter };
